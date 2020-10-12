73241b3ad9730ddb8f79c644e7d58a39ec1cbce6
JHOU YU SYUAN <49354736+JHOUJHOU@users.noreply.github.com>
2020-10-12 23:06:57 +0800

    Delete all1.js

--- all1.js
+++
@@ -1,9 +0,0 @@
-
-function login() {
-    var newstuser = "109";
-    var stuser = document.getElementById("username").value;
-    var stpass = document.getElementById("password").value;
-    if (stuser == newstuser) {
-        alert('登入成功');    
-    }
-}
